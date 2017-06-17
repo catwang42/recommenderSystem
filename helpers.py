ad9b01867213d238a7d4bce7907c78ca4b798d2b
catwang89 <catwang89@gmail.com>
2017-06-17 11:33:32 +1000

    init

---
+++ helpers.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+
+import re
+def filterPid(res):
+	res_pid = []  
+	pids = re.findall(r'(?<=\<).+?(?=\>)',res)
+	for pid in pids:
+		res_pid.append(pid.upper())
+	return res_pid
